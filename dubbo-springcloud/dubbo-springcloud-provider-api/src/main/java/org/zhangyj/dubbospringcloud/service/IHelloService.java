d413189d83bf04867e826f66dd3a29b0e1fbef22
zhangyj <10989822863@qq.com>
2020-12-06 22:41:19 +0800

    添加service接口

---
+++ dubbo-springcloud/dubbo-springcloud-provider-api/src/main/java/org/zhangyj/dubbospringcloud/service/IHelloService.java
@@ -0,0 +1,14 @@
+package org.zhangyj.dubbospringcloud.service;
+
+/**
+ * @author ZHANG-YJ
+ */
+public interface IHelloService {
+
+    /**
+     * sayHello方法
+     * @param name 名称
+     * @return 问候语
+     */
+    String sayHello(String name);
+}
