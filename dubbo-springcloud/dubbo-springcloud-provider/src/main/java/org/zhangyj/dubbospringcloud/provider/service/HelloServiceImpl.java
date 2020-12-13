dec558c42fd6c9f3b51861b872fe95a852035c28
zhangyj <10989822863@qq.com>
2020-12-13 22:47:06 +0800

    添加service接口

--- dubbo-springcloud/dubbo-springcloud-provider/src/main/java/org/zhangyj/dubbospringcloud/provider/service/HelloServiceImpl.java
+++ dubbo-springcloud/dubbo-springcloud-provider/src/main/java/org/zhangyj/dubbospringcloud/provider/service/HelloServiceImpl.java
@@ -1,12 +1,12 @@
 package org.zhangyj.dubbospringcloud.provider.service;
 
-import org.apache.dubbo.config.annotation.Service;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.zhangyj.dubbospringcloud.providerapi.service.IHelloService;
 
 /**
  * @author ZHANG-YJ
  */
-@Service
+@DubboService
 public class HelloServiceImpl implements IHelloService {
 
     public String sayHello(String name) {
